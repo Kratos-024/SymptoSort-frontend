d5ce5e5cf2b84e5628efd8b5435b6026cc687a1d
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-07-19 18:46:39 +0530

    removed component

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAeuQY8Fav3LtkdkqO_C4p36b65mM1Bgss",
   authDomain: "dfgf-b7995.firebaseapp.com",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-13BX99VQCW",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
